b0192790ad11c0a435b62756f0982592ce81e3a7
nhthlinh <linhnguyen.8023@gmail.com>
2025-05-15 22:08:46 +0700

    fix to deploy

---
+++ server.js
@@ -0,0 +1,15 @@
+const { spawn } = require("child_process");
+
+const server = spawn("php", ["artisan", "serve", "--host=127.0.0.1", "--port=8000"]);
+
+server.stdout.on("data", (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+server.stderr.on("data", (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+server.on("close", (code) => {
+  console.log(`Laravel server exited with code ${code}`);
+});
